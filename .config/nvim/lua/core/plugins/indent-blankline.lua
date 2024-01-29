456342d152f8167fc6f70535ad987988a76de005
Cristian Betivu <cbe@languagewire.com>
2024-01-29 20:41:20 +0200

    Update nvim packages

--- .config/nvim/lua/core/plugins/indent-blankline.lua
+++ .config/nvim/lua/core/plugins/indent-blankline.lua
@@ -9,14 +9,8 @@ local M = {
         vim.opt.listchars:append("extends:>")
         vim.opt.listchars:append("precedes:<")
         vim.opt.listchars:append("space:·")
-        require("indent_blankline").setup({
-            -- for example, context is off by default, use this to turn it on
-            show_current_context = true,
-            show_current_context_start = true,
-            show_end_of_line = true,
-            space_char_blankline = " ",
-            show_trailing_blankline_indent = false,
-            char = "┊",
+        require("ibl").setup({
+            indent = { char = "┊" },
         })
     end,
 }
