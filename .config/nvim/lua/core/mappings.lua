61b4e01387027bb0d950066403dba2cea769e176
Cristian B <cbetivu@gmail.com>
2024-12-21 20:20:57 +0200

    Custom search function in nvim to allow file filtering

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -22,9 +22,56 @@ end, { desc = "[/] Fuzzily search in current buffer" })
 vim.keymap.set("n", "<C-p>", function()
     telescope_builtins.find_files({ hidden = true })
 end, { desc = "[S]earch [F]iles" })
+
+
+local live_grep_with_glob = function (opts)
+    local pickers = require"telescope.pickers"
+    local finders = require"telescope.finders"
+    local make_entry = require"telescope.make_entry"
+    local conf = require"telescope.config".values
+    opts = opts or {}
+    opts.cwd = opts.cwd or vim.uv.cwd()
+
+    local finder = finders.new_async_job {
+        command_generator = function (prompt)
+            if not prompt or prompt == "" then
+                return nil
+            end
+
+            local pieces = vim.split(prompt, "  ")
+            local args = { "rg" }
+            if pieces[1] then
+                table.insert(args, "-e")
+                table.insert(args, pieces[1])
+            end
+
+            if pieces[2] then
+                table.insert(args, "-g")
+                table.insert(args, pieces[2])
+            end
+
+            return  vim.iter({
+                args,
+                {"--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case"}
+            }):flatten():totable()
+        end,
+        entry_maker = make_entry.gen_from_vimgrep(opts),
+        cwd = opts.cwd,
+    }
+
+    pickers.new(opts, {
+        debounce = 100,
+        prompt_title = "Live Grep (+glob)",
+        finder = finder,
+        previewer = conf.grep_previewer(opts),
+        sorter = require("telescope.sorters").empty(),
+    }):find()
+end
+
 vim.keymap.set("n", "<leader>sh", telescope_builtins.help_tags, { desc = "[S]earch [H]elp" })
 vim.keymap.set("n", "<leader>sw", telescope_builtins.grep_string, { desc = "[S]earch current [W]ord" })
-vim.keymap.set("n", "<leader>sg", telescope_builtins.live_grep, { desc = "[S]earch by [G]rep" })
+-- vim.keymap.set("n", "<leader>sg", telescope_builtins.live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>sg", live_grep_with_glob, { desc = "[S]earch by [G]rep" })
 vim.keymap.set("n", "<leader>sd", telescope_builtins.diagnostics, { desc = "[S]earch [D]iagnostics" })
 
 -- Diagnostic keymaps
