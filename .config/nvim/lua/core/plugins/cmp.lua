2f07dbcc5727c1f08011ca400f27c9eb0460ca98
Cristian B <cbetivu@gmail.com>
2025-01-04 15:19:02 +0200

    Replace deprecated function

--- .config/nvim/lua/core/plugins/cmp.lua
+++ .config/nvim/lua/core/plugins/cmp.lua
@@ -1,9 +1,8 @@
 -- better completion support
-
 -- functions for super tab like support
 -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#vim-vsnip
 local has_words_before = function()
-    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+    if vim.api.nvim_get_option_value("buftype", {buf = 0}) == "prompt" then
         return false
     end
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
